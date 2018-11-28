387d4c2c393984742f7cda570c0167cf2d76f64a
codebaker <jungah.kim76@gmail.com>
2018-11-28 17:40:35 +0900

    ...

--- app/src/main/java/com/womenproiot/www/link/LoadingActivity.java
+++ app/src/main/java/com/womenproiot/www/link/LoadingActivity.java
@@ -3,10 +3,7 @@ package com.womenproiot.www.link;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -15,7 +12,6 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        //animate();
         startLoading();
     }
 
@@ -27,12 +23,6 @@ public class LoadingActivity extends AppCompatActivity {
                 finish();
             }
         }, 3000);
-    }
-
-    private void animate() {
-        ImageView rabbit = (ImageView) findViewById(R.id.intro);
-        GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(rabbit);
-        Glide.with(this).load(R.drawable.intro).into(gifImage);
     }
 
     @Override
